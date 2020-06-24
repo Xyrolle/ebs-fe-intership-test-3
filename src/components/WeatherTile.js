9977271b78e55915993b292702a63f8af7af82e8
Serghey <serii.hmeli123123@mail.ru>
2020-06-24 11:53:49 +0300

    router bag fix

--- src/components/WeatherTile.js
+++ src/components/WeatherTile.js
@@ -1,32 +1,24 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function WeatherTile({ temp, feels_like, main, date, iconURL }) {
 	const day = date.substring(0, 3),
 		time = date.substring(15, 24);
+
 	return (
-		<Router>
-			<Link
-				to={`/${day}`}
-				onClick={() => {
-					setTimeout(() => {
-						window.location.reload();
-					}, 100);
-				}}
-			>
-				<div className='weather-tile'>
-					<div className='day'>{day}</div>
-					<img src={iconURL} alt='weather-img' />
-					<div>
-						{temp} ℃ / {feels_like} ℃
-					</div>
-					<div className='main'>{main}</div>
-					<div>{time}</div>
+		<Link to={`/${day}`}>
+			<div className='weather-tile'>
+				<div className='day'>{day}</div>
+				<img src={iconURL} alt='weather-img' />
+				<div>
+					{temp} ℃ / {feels_like} ℃
 				</div>
-			</Link>
-		</Router>
+				<div className='main'>{main}</div>
+				<div>{time}</div>
+			</div>
+		</Link>
 	);
 }
 
